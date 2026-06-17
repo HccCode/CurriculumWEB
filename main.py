738704afba046b7a9d529491d885a03022ec45e6
HectorUM <hector7x@hotmail.com>
2026-06-16 21:09:29 -0700

    2

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Depends, HTTPException, status
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
 import json
 import os
+import secrets
 
 app = FastAPI()
+security = HTTPBasic()
 
-# Configuramos rutas absolutas para no perdernos en carpetas
+# Rutas absolutas
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DATA_FILE = os.path.join(BASE_DIR, "data.json")
 STATIC_DIR = os.path.join(BASE_DIR, "static")
@@ -16,11 +19,28 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, "templates")
 app.mount("/static", StaticFiles(directory=STATIC_DIR), name="static")
 templates = Jinja2Templates(directory=TEMPLATES_DIR)
 
+# --- CREDENCIALES DE ADMINISTRADOR ---
+ADMIN_USERNAME = "admin"
+ADMIN_PASSWORD = "secreto123"
+
 def get_data():
     if not os.path.exists(DATA_FILE):
-        return {"error": "data.json no encontrado"}
+        return {}
     with open(DATA_FILE, "r", encoding="utf-8") as f:
         return json.load(f)
+
+# Función para verificar usuario y contraseña
+def verify_credentials(credentials: HTTPBasicCredentials = Depends(security)):
+    correct_username = secrets.compare_digest(credentials.username, ADMIN_USERNAME)
+    correct_password = secrets.compare_digest(credentials.password, ADMIN_PASSWORD)
+    
+    if not (correct_username and correct_password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Correo o contraseña incorrectos",
+            headers={"WWW-Authenticate": "Basic"},
+        )
+    return credentials.username
 
 @app.get("/", response_class=HTMLResponse)
 async def read_cv(request: Request):
@@ -30,8 +50,9 @@ async def read_cv(request: Request):
         context={"data": get_data()}
     )
 
+# Ruta protegida con Dependencia (pedirá usuario y contraseña)
 @app.get("/admin", response_class=HTMLResponse)
-async def read_admin(request: Request):
+async def read_admin(request: Request, username: str = Depends(verify_credentials)):
     return templates.TemplateResponse(
         request=request, 
         name="admin.html", 
@@ -44,8 +65,6 @@ async def save_data(request: Request):
         new_data = await request.json()
         with open(DATA_FILE, "w", encoding="utf-8") as f:
             json.dump(new_data, f, indent=4, ensure_ascii=False)
-        print("Archivo data.json guardado exitosamente.")
         return {"status": "success"}
     except Exception as e:
-        print(f"ERROR AL GUARDAR: {e}")
         return {"status": "error", "message": str(e)}
